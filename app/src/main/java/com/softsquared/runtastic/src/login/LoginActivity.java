a152f199ee380d7db98bad39f716f907f22297f5
ckgod <61004564+ckgod@users.noreply.github.com>
2020-02-16 21:53:14 +0900

    mainFragment

--- app/src/main/java/com/softsquared/runtastic/src/login/LoginActivity.java
+++ app/src/main/java/com/softsquared/runtastic/src/login/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.softsquared.runtastic.src.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.softsquared.runtastic.R;
 import com.softsquared.runtastic.src.BaseActivity;
 import com.softsquared.runtastic.src.login.interfaces.LoginActivityView;
+import com.softsquared.runtastic.src.main.MainActivity;
 
 public class LoginActivity extends BaseActivity implements LoginActivityView {
 
@@ -21,9 +23,16 @@ public class LoginActivity extends BaseActivity implements LoginActivityView {
                 finish();
                 break;
             case R.id.login_btn_login:
+                redirectMainActivity();
+                finish();
                 break;
             default:
                 break;
         }
+    }
+
+    private void redirectMainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/softsquared/runtastic/src/main/MainActivity.java
+++ app/src/main/java/com/softsquared/runtastic/src/main/MainActivity.java
@@ -1,24 +1,74 @@
 package com.softsquared.runtastic.src.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.softsquared.runtastic.R;
 import com.softsquared.runtastic.src.BaseActivity;
+import com.softsquared.runtastic.src.main.fragment.FragmentAct;
+import com.softsquared.runtastic.src.main.fragment.FragmentNewsPeed;
+import com.softsquared.runtastic.src.main.fragment.FragmentPlan;
+import com.softsquared.runtastic.src.main.fragment.FragmentProfile;
+import com.softsquared.runtastic.src.main.fragment.FragmentStatus;
 import com.softsquared.runtastic.src.main.interfaces.MainActivityView;
 
 public class MainActivity extends BaseActivity implements MainActivityView {
     private TextView mTvHelloWorld;
+
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+    private FragmentAct mFragmentAct = new FragmentAct();
+    private FragmentNewsPeed mFragmentNewsPeed = new FragmentNewsPeed();
+    private FragmentPlan mFragmentPlan = new FragmentPlan();
+    private FragmentProfile mFragmentProfile = new FragmentProfile();
+    private FragmentStatus mFragmentStatus = new FragmentStatus();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTvHelloWorld = findViewById(R.id.main_tv_hello_world);
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        transaction.replace(R.id.main_frameLayout, mFragmentNewsPeed).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.main_bottom_nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.menu_news_peed:
+                    transaction.replace(R.id.main_frameLayout, mFragmentNewsPeed).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_status:
+                    transaction.replace(R.id.main_frameLayout, mFragmentStatus).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_act:
+                    transaction.replace(R.id.main_frameLayout, mFragmentAct).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_plan:
+                    transaction.replace(R.id.main_frameLayout, mFragmentPlan).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_profile:
+                    transaction.replace(R.id.main_frameLayout, mFragmentProfile).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 
     private void tryGetTest() {
@@ -42,9 +92,8 @@ public class MainActivity extends BaseActivity implements MainActivityView {
 
     public void customOnClick(View view) {
         switch (view.getId()) {
-            case R.id.main_btn_hello_world:
-                tryGetTest();
-                break;
+           // case :
+              //  break;
             default:
                 break;
         }
